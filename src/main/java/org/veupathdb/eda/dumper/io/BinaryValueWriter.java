7bb9a14799a0800b6341728ed270bcbd52e03d91
Dan Galdi <dmgaldi@upenn.edu>
2022-06-08 10:06:37 -0400

    Writing and reading files example code

---
+++ src/main/java/org/veupathdb/eda/dumper/io/BinaryValueWriter.java
@@ -0,0 +1,30 @@
+package org.veupathdb.eda.dumper.io;
+
+import org.veupathdb.service.eda.ss.model.variable.converter.BinarySerializer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class BinaryValueWriter<T> implements AutoCloseable {
+
+  private final OutputStream outputStream;
+  private final BinarySerializer<T> binarySerializer;
+
+  public BinaryValueWriter(final OutputStream outputStream, final BinarySerializer<T> byteConverter) {
+    this.outputStream = outputStream;
+    this.binarySerializer = byteConverter;
+  }
+
+  public void writeValue(T variable) {
+    try {
+      outputStream.write(binarySerializer.toBytes(variable));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void close() throws Exception {
+    this.outputStream.close();
+  }
+}
