44f22c966b60692ff483d79c6eb17fcb53409565
Dan Galdi <dmgaldi@upenn.edu>
2023-11-30 14:32:35 -0500

    Fix

--- src/main/java/org/veupathdb/eda/binaryfiles/dumper/VariableFilesStringDumper.java
+++ src/main/java/org/veupathdb/eda/binaryfiles/dumper/VariableFilesStringDumper.java
@@ -6,6 +6,8 @@ import org.json.JSONArray;
 import org.veupathdb.eda.binaryfiles.BinaryValueWriter;
 import org.veupathdb.service.eda.ss.model.Entity;
 import org.veupathdb.service.eda.ss.model.Study;
+import org.veupathdb.service.eda.ss.model.variable.LongitudeVariable;
+import org.veupathdb.service.eda.ss.model.variable.VariableType;
 import org.veupathdb.service.eda.ss.model.variable.VariableValueIdPair;
 import org.veupathdb.service.eda.ss.model.variable.VariableWithValues;
 import org.veupathdb.service.eda.ss.model.variable.binary.BinaryFilesManager;
@@ -15,6 +17,8 @@ import org.veupathdb.service.eda.ss.model.variable.binary.StringValueConverter;
 import org.veupathdb.service.eda.ss.model.variable.binary.ValueWithIdSerializer;
 
 import java.io.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -69,6 +73,17 @@ public class VariableFilesStringDumper<T> implements FilesDumper {
     if (row.get(_valColumnIndex).length() > maxLen - 4) {
       LOG.error("Failed to dump " + row + " var " + _valueVar.getId());
     }
-    _varFileWriter.writeValue(new VariableValueIdPair<>(idIndex, row.get(_valColumnIndex)));
+    if (_valueVar.getType() == VariableType.LONGITUDE) {
+      Double d = Double.valueOf(row.get(_valColumnIndex));
+      BigDecimal bd = BigDecimal.valueOf(d);
+      LongitudeVariable var = (LongitudeVariable) _valueVar;
+      bd = bd.setScale(Math.min(bd.precision() - 1, var.getPrecision().intValue() - 2), RoundingMode.HALF_UP);
+      if (bd.precision() == var.getPrecision().intValue() - 2) {
+        LOG.info("Writing max precision: " + var.getId());
+      }
+      _varFileWriter.writeValue(new VariableValueIdPair<>(idIndex, bd.toString()));
+    } else {
+      _varFileWriter.writeValue(new VariableValueIdPair<>(idIndex, row.get(_valColumnIndex)));
+    }
   }
 }
